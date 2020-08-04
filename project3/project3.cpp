ed0d1a2e082349e70221533d3232539df6b2f1e2
Rachel Bennett <rachel.l.bennett-1@ou.edu>
2020-08-04 09:46:21 -0500

    Finished random number generator

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 class sortElements {
+
+	friend ostream& operator<< (ostream& s, sortElements& ele);
+
 protected:
 	int numElements; //Number of elements of the array
 	int* elements; //dynamic array with the elements of type int
@@ -36,4 +39,37 @@ sortElements::~sortElements() {
 	if (elements != NULL) delete[] elements;
 	elements = NULL;
 	numElements = 0;
+}
+
+void sortElements::generateRandom(int seed, int lower, int upper) {
+	srand(seed); //Sets the seed for the random number generator
+	
+	for (int i = 0; i < numElements; i++) {
+		elements[i] = (rand() % upper) + lower; //Generate random number between upper and lower and fill array
+	}
+}
+
+ostream& operator << (ostream& s, sortElements& ele) { //This line taken from pg 146 of book
+
+	for (int i = 0; i < ele.numElements; i++)
+	{
+		if (i > 0)
+		{
+			s << ' '; //Prints a space between all elements, but not before first
+		}
+		s << ele.elements[i]; //Prints the given element
+	}
+	s << endl; //Ends with an endline
+
+	return s;
+}
+
+int main() {
+
+	sortElements* a = new sortElements(5);
+	a->generateRandom(1, 0, 20);
+
+	cout << *a;
+
+	return 0;
 }
