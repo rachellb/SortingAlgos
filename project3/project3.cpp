61ee529801c723dd903138af828c0aa839fc077d
Rachel Bennett <rachel.l.bennett-1@ou.edu>
2020-08-04 10:46:58 -0500

    Finished bubble and quick sort

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+void swap(int* x, int* y); //For swapping elements of array
+int partition(int* ele, int low, int high); //For partitionaing array in quick sort
+
 class sortElements {
 
 protected:
@@ -18,8 +21,9 @@ public:
 	void displayElements(int* arr); // display the given set
 	int* getElementsArray(); // return the entire array of elements
 	int getnumElements(); // return the number of elements
-	// Necessary methods for each of the sorting algorithm
-	// - Bubble and Quick sort methods
+	void bubbleSort();
+	void quickSort(int* ele, int low, int high);
+	
 	// - Extra credit – Shell and Adaptive sort methods
 };
 
@@ -67,6 +71,30 @@ int sortElements::getnumElements() {
 	return numElements;
 }
 
+void sortElements::bubbleSort()
+{
+	int i, j;
+	for (i = 0; i < numElements -1; i++)
+
+		// Last i elements are already in place  
+		for (j = 0; j < numElements - i - 1; j++)
+			if (elements[j] > elements[j + 1])
+				swap(&elements[j], &elements[j + 1]);
+}
+
+void sortElements::quickSort(int* ele, int low, int high)
+{
+	if (low < high)
+	{
+		int p = partition(ele, low, high); //Partition the array
+
+		// Separately sort elements before  
+		// partition and after partition  
+		quickSort(ele, low, p - 1);
+		quickSort(ele, p + 1, high);
+	}
+}
+
 int main() {
 
 	sortElements* a = new sortElements(5);
@@ -74,5 +102,34 @@ int main() {
 
 	a->displayElements(a->getElementsArray());
 
+	a->quickSort(a->getElementsArray(),0,a->getnumElements()-1);
+
+	a->displayElements(a->getElementsArray());
+
 	return 0;
+}
+
+void swap(int* x, int* y)
+{
+	int temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int partition(int* ele, int low, int high)
+{
+	int pivot = ele[high]; // pivot  
+	int i = (low - 1); // Index of smaller element  
+
+	for (int j = low; j <= high - 1; j++)
+	{
+		// If current element is smaller than the pivot  
+		if (ele[j] < pivot)
+		{
+			i++; // increment index of smaller element  
+			swap(&ele[i], &ele[j]);
+		}
+	}
+	swap(&ele[i + 1], &ele[high]);
+	return (i + 1);
 }
